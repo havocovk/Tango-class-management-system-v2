13175a4649a9f0d47cbd5091ddeea52d1bd435ac
Orhan Veli Kazanci <orhanveli.kazanci@gmail.com>
2026-07-14 18:05:24 +0300

    Update 90

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 //   3. Cache'te yoksa ağa git, cevabı cache'e ekle
 // ---------------------------------------------------------------
 
-const CACHE_NAME = 'tcms-v163';
+const CACHE_NAME = 'tcms-v164';
 
 // İlk kurulumda cache'lenecek dosyalar (uygulama kabuğu)
 const SHELL_FILES = [
